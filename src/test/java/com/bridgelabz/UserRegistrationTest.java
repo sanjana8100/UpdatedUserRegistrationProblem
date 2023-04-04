07eab726398a3b15392938273d6f791b44c61941
sanjana8100 <sanjanams812@gmail.com>
2023-04-04 17:11:14 +0530

    added: UC11

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,16 +1,8 @@
 package com.bridgelabz;
 
 import org.junit.jupiter.api.*;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-import java.util.Arrays;
-import java.util.Collection;
-
-@RunWith(Parameterized.class)
 public class UserRegistrationTest {
-    private String email;
-    private boolean expectedResult;
     static UserRegistration userRegistration;
 
     @BeforeAll
@@ -49,36 +41,5 @@ public class UserRegistrationTest {
         boolean Password = userRegistration.validatePassword("Sanjana@1");
         Assertions.assertTrue(Password);
         System.out.println("Password Checked(Rule1, Rule2, Rule3 & Rule4) :- Successfully Passed UC5,UC6,UC7,UC8");
-    }
-
-    public UserRegistrationTest(String email, boolean expectedResult) {
-        this.email = email;
-        this.expectedResult = expectedResult;
-    }
-
-    @Parameterized.Parameters
-    public static Collection sampleEmails(){
-        return Arrays.asList(new Object[][]{
-                {"abc@yahoo.com", true},
-                {"abc", false},
-                {"abc-100@yahoo.com", true},
-                {"abc111@abc.com", true},
-                {"abc()*@gmail.com", false},
-                {"abc-100@abc.net", true},
-                {"abc.100@abc.com.au", true},
-                {"abc@1.com", true},
-                {"abc@gmail.com.aa.au", false},
-                {"abc@gmail.com.com", true},
-                {"abc+100@gmail.com", true},
-                {"abc@yahoo.com", true}
-        });
-    }
-
-
-    @Test
-    void checkEmailShouldReturnTrue(){
-        System.out.println("Parameterized Sample Email is: "+email);
-        Assertions.assertEquals(expectedResult,userRegistration.validateEmail(email));
-        System.out.println("Email Sample Passed");
     }
 }
